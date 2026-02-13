94130dfce9a9e9e9c18bcbbc7ab68aac9b317e39
eleanor-65 <e.brendon@outlook.com>
2026-02-13 19:38:37 +0000

    changed euros to dollars

--- CurrencyConverter.java
+++ CurrencyConverter.java
@@ -1,17 +1,17 @@
 public class CurrencyConverter {
     public static void main(String[] args) {
         java.util.Scanner scan = new java.util.Scanner(System.in);
-        System.out.print("If I give you £1, how many Euro will you give me? ");
-        double euroOverPoundRatio = scan.nextDouble();
-        System.out.print("If I give you 1€, how many Sterling pounds will you give me? ");
-        double poundOverEuroRatio = scan.nextDouble();
+        System.out.print("If I give you £1, how many dollars will you give me? ");
+        double dollarOverPoundRatio = scan.nextDouble();
+        System.out.print("If I give you $1, how many Sterling pounds will you give me? ");
+        double poundOverDollarRatio = scan.nextDouble();
         System.out.println("OK, now I know enough.");
         boolean finished = false;
         while (!finished) {
             System.out.println();
             System.out.println("What would you like to do?");
-            System.out.println("   1 - Exchange pounds into euro");
-            System.out.println("   2 - Exchange euro into pounds");
+            System.out.println("   1 - Exchange pounds into dollars");
+            System.out.println("   2 - Exchange dollars into pounds");
             System.out.println("   0 - Exit the program");
             System.out.println();
             System.out.print(  "> ");
@@ -20,14 +20,14 @@ public class CurrencyConverter {
             case 1:
                 System.out.print("How many pounds would you like to convert? ");
                 double pounds = scan.nextDouble();
-                double euro   = pounds * euroOverPoundRatio;
-                System.out.println("£" + pounds + " will give you " + euro + "€");
+                double dollars   = pounds * dollarOverPoundRatio;
+                System.out.println("£" + pounds + " will give you $" + dollars);
                 break;
             case 2:
-                System.out.print("How many euro would you like to convert? ");
-                euro   = scan.nextDouble();
-                pounds = euro * poundOverEuroRatio;
-                System.out.println(euro + "€ will give you £" + pounds);
+                System.out.print("How many dollars would you like to convert? ");
+                dollars   = scan.nextDouble();
+                pounds = dollars * poundOverDollarRatio;
+                System.out.println("$"+ dollars + " will give you £" + pounds);
                 break;
             case 0:
                 finished = true;
